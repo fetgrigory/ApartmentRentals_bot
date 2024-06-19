9f5e4d719b2619575926ac93cf3a670772493124
fetgrigory <157891679+fetgrigory@users.noreply.github.com>
2024-06-19 17:44:22 +0300

    Commit

--- main.py
+++ main.py
@@ -131,15 +131,13 @@ async def add_apartment_video(message: types.Message, state: FSMContext):
 @dp.callback_query_handler()
 async def callback_query_viewing_atelier(callback_query: types.CallbackQuery):
     if callback_query.data == 'atelier':
-        apartment = await db.get_apartment_by_type('atelier')
-        if apartment:
-            apartment = apartment[0]
+        apartments = await db.get_apartment_by_type('atelier')
+        for apartment in apartments:
             video = apartment[4]
             caption = f"Адрес: {apartment[1]}\nОписание: {apartment[2]}\nЦена: {apartment[3]}"
             await bot.send_video(chat_id=callback_query.from_user.id, video=video, caption=caption, reply_markup=kb.product_list)
     if callback_query.data == 'selected_apartment':
         message_id = callback_query.message.message_id
         await bot.edit_message_reply_markup(chat_id=callback_query.from_user.id, message_id=message_id, reply_markup=kb.pay_list)
-# Starting the polling loop
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
